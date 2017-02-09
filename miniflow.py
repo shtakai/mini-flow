e9afdb709e5d28e253b236db2f8bc3f1dc90d7c1
Kausthub Raj Jadhav <kjadhav@uci.edu>
2017-02-08 22:10:09 -0800

    Add node description :pencil:

--- miniflow.py
+++ miniflow.py
@@ -1,3 +1,8 @@
 '''
-class Node(object):
+Each node recieves inputs from multiple other nodes.
+Each node creates a single output, which will be passed to other nodes
+
 '''
+class Node(object):
+	def __init__(self):
+		
