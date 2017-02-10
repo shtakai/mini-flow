ae5156ccd9b390b647bd5ea01e6efca5dd3dd0b9
Kausthub Raj Jadhav <kjadhav@uci.edu>
2017-02-09 17:59:47 -0800

    Update nn test :arrow_up:

--- nn.py
+++ nn.py
@@ -6,7 +6,11 @@ from miniflow import *
 
 x, y = Input(), Input()
 
+f = Add(x, y, z)
+
 feed_dict = {x: 10, y: 5}
 
 sorted_nodes = topological_sort(feed_dict)
 output = forward_pass(f, sorted_nodes)
+
+print("{} + {} = {} (according to miniflow)".format(feed_dict[x], feed_dict[y], output))
