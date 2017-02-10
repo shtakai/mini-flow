482fae00f725a350abcbc496a4e13d1e33ef8d7e
Kausthub Raj Jadhav <kjadhav@uci.edu>
2017-02-09 17:52:33 -0800

    Add test :white_check_mark:

--- nn.py
+++ nn.py
@@ -1,4 +1,12 @@
 # nn.py
 # This file describes the Nueral Net Architecture
+# This script builds and runs a graph with miniflow.
 
+from miniflow import *
 
+x, y = Input(), Input()
+
+feed_dict = {x: 10, y: 5}
+
+sorted_nodes = topological_sort(feed_dict)
+output = forward_pass(f, sorted_nodes)
